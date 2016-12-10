dcf851f42e7a03f931b407b28e26dfc39d305f8d
Jason Kung <lovepage1212@gmail.com>
2016-12-11 02:36:50 +0800

    Support system setting for rotation screen.

--- src/com/jasonkung/launcher3/SettingsActivity.java
+++ src/com/jasonkung/launcher3/SettingsActivity.java
@@ -22,11 +22,36 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.provider.Settings.System;
 
 /**
  * Settings activity for Launcher. Currently implements the following setting: Allow rotation
  */
 public class SettingsActivity extends Activity {
+
+    class SystemDisplayRotationLockObserver extends ContentObserver {
+        private final ContentResolver mResolver;
+        private final Preference mRotationPref;
+
+        public SystemDisplayRotationLockObserver(Preference preference, ContentResolver contentResolver) {
+            super(new Handler());
+            this.mRotationPref = preference;
+            this.mResolver = contentResolver;
+        }
+
+        public void onChange(boolean z) {
+            boolean enable = true;
+            if (System.getInt(this.mResolver, "accelerometer_rotation", 1) != 1) {
+                enable = false;
+            }
+            this.mRotationPref.setEnabled(enable);
+            this.mRotationPref.setSummary(enable ? R.string.allow_rotation_desc : R.string.allow_rotation_blocked_desc);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +65,9 @@ public class SettingsActivity extends Activity {
     /**
      * This fragment shows the launcher preferences.
      */
-    public static class LauncherSettingsFragment extends PreferenceFragment
+    public class LauncherSettingsFragment extends PreferenceFragment
             implements OnPreferenceChangeListener {
+            private SystemDisplayRotationLockObserver mRotationLockObserver;
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -60,8 +86,26 @@ public class SettingsActivity extends Activity {
             pref.setChecked(value.getBoolean(LauncherSettings.Settings.EXTRA_VALUE));
 
             pref.setOnPreferenceChangeListener(this);
+
+
+            ContentResolver contentResolver = getActivity().getContentResolver();
+            mRotationLockObserver = new SystemDisplayRotationLockObserver(pref, contentResolver);
+            contentResolver.registerContentObserver(System.getUriFor("accelerometer_rotation"), false, mRotationLockObserver);
+            this.mRotationLockObserver.onChange(true);
+            pref.setDefaultValue(Boolean.valueOf(Utilities.getAllowRotationDefaultValue(getActivity())));
+
+
         }
 
+        public void onDestroy() {
+            if (this.mRotationLockObserver != null) {
+                getActivity().getContentResolver().unregisterContentObserver(this.mRotationLockObserver);
+                this.mRotationLockObserver = null;
+            }
+            super.onDestroy();
+        }
+
+        //TODO: no use ..
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
             Bundle extras = new Bundle();
